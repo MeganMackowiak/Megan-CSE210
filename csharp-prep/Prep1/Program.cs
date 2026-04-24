75ed995f8a6464aaa63a191cc047b9d7d546546d
MeganMackowiak <mac24027@byui.edu>
2026-04-23 21:01:42 -0600

    4/23/2026

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string firstName;
+        firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        String lastName = Console.ReadLine();
+
+        Console.WriteLine($"{lastName}, {firstName}{lastName}"); 
+    
     }
 }
