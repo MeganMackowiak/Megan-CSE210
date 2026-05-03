89703a5a11fed15ea271cf82a9f44754769d3fe2
MeganMackowiak <mac24027@byui.edu>
2026-05-02 23:10:48 -0600

    Prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Text.Json.Serialization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcomeMessage();
+
+       string userName = PromptUserName();
+
+       int userNumber = PromptUserNumber();
+
+       int squaredNumber = SquaredNumber(userNumber);
+
+       int birthYear; 
+
+       PromptUserBirthYear(out birthYear);
+
+       DisplayResult(userName, squaredNumber, birthYear);
+    }
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the program!! ");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please input your name: ");
+        int number = int.Parse(Console.ReadLine());
+        return number; 
+    }
+
+    static void PromptUserBirthYear(out int birthYear)
+    {
+        Console.write($"Enter the year you were born: ");
+        birthYear = int.Parse(Console.Readline());
+    }
+
+    static int SquaredNumber(int number)
+    {
+        int square = number * number; 
+        return square; 
+    }
+
+    static void Displayresults(string name, int square, int birthYear)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}.);
+        Console.WriteLine($"{name}, you will turn {2026- birthyear} years old this year.");
     }
 }
